ebf06f012609e85a6a55ee1822a80711292b3d11
Nobody0Zy <1142632191@qq.com>
2023-08-25 10:46:22 +0800

    test pass! update feature/compose_date_bar

--- compose_and_evaluate_date_bar/compose_date_bar/compose_date_bar_by_format_min_bar.py
+++ compose_and_evaluate_date_bar/compose_date_bar/compose_date_bar_by_format_min_bar.py
@@ -6,7 +6,7 @@ from typing import Dict, List, Set, Tuple, Union
 import numpy as np
 import pandas as pd
 
-sys.path.append(r"D:\QUANT_GAME\python_game\pythonProject")
+sys.path.append(r"D:\\QUANT_GAME\\python_game\\pythonProject")
 from my_tools_packages import MyDecorator as MD
 
 running_time = MD.running_time
@@ -14,10 +14,12 @@ running_time = MD.running_time
 import logging
 
 logging.basicConfig(
-    file_name='compose_date_bar_by_format_min_bar.log',
-    level=logging.INFO,
+    filename='error_min_bar_stk_df.log',
+    level=logging.ERROR,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    datefmt = '%Y-%m-%d %H:%M:%S')
+    datefmt='%Y-%m-%d %H:%M:%S',
+    # filemode='w+'
+    )
 
 sys.path.append(r"D:\\QUANT_GAME\\python_game\\pythonProject\\hk_stk_data_processing_codes_by_jsy\\common")
 import config
@@ -31,10 +33,13 @@ def _gen_daily_price_data_from_min_bar_stk_df(min_bar_stk_df):
     
     min_bar_stk_df_open_dropna = min_bar_stk_df['open'].dropna()
     if min_bar_stk_df_open_dropna.empty:
-        logging.error('min_bar_stk_df的开盘价全为nan值，数据可能有误，请检查, min_bar_stk_df:\n%s',min_bar_stk_df.index[0])
+        logging.error('min_bar_stk_df的开盘价全为nan值，数据可能有误，请检查, min_bar_stk_df:\n%s',min_bar_stk_df)
+        open_price = np.nan
+    else:
+        open_price = min_bar_stk_df_open_dropna.iloc[0]
     # 生成日线数据
     date_price_volume_data_dict = {
-        'open': min_bar_stk_df_open_dropna.iloc[0],
+        'open': open_price,
         'high': min_bar_stk_df['high'].max(),
         'low': min_bar_stk_df['low'].min(),
         'close': min_bar_stk_df['close'].iloc[-1],
@@ -58,8 +63,6 @@ def process_file(file_path):
     """ 
     处理单个文件
     """
-    file_name = os.path.basename(file_path)
-    logging.info(f'processing {file_name}...')
     min_bar_df = pd.read_pickle(file_path)
     date_bar_df = compose_date_bar_by_format_min_bar(min_bar_df)
     return date_bar_df
@@ -82,11 +85,14 @@ def save_date_bar_df(date_bar_df_total,save_folder_path):
     date_bar_df_total.to_pickle(save_folder_path)
     logging.info(f'save date_bar_df_total to {save_folder_path} successfully!')   
 
-
-
+@running_time
 def main():
-    MIN_BAR_FOLDER_PATH = config.min_bar_develop_hist_folder_path['v10']
-    DATE_BAR_FOLDER_PATH = config.date_bar_develop_hist_folder_path['v10']
+    # 开发历史分钟线数据（存储于机械硬盘）
+    # MIN_BAR_FOLDER_PATH = config.min_bar_develop_hist_folder_path['v10']
+    # 开发临时分钟线数据（存储于固态硬盘，提升读写速度）
+    MIN_BAR_FOLDER_PATH = config.min_bar_temp_folder_path['v10']
+    # 合成日线存储路径，历史开发路径，存储于机械硬盘
+    DATE_BAR_FOLDER_PATH = config.date_bar_develop_hist_file_path['v10']
     num_process = 10
     
     need_compose_min_bar_file_path_list = get_file_paths(MIN_BAR_FOLDER_PATH)
@@ -96,7 +102,7 @@ def main():
     
     date_bar_df_total = pd.concat(date_bar_df_list)
     save_date_bar_df(date_bar_df_total,DATE_BAR_FOLDER_PATH)
-    
+    logging.info('all done!')
     
 if __name__ == '__main__':
     print('The game is on!')
